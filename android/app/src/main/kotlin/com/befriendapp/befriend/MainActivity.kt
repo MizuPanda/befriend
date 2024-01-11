6b1246eb71ca89b947802beea4e24f5ad8c80f64
Juniel Djossou <junieldjossou@v1041-wn-rt-b-160-113.campus-dynamic.uwaterloo.ca>
2024-01-10 19:12:18 -0500

    fixed ios hopefully

---
+++ android/app/src/main/kotlin/com/befriendapp/befriend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.befriendapp.befriend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ befriend/android/app/src/main/kotlin/com/example/befriend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.befriend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
