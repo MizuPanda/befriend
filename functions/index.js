cd15ce7b1ad78334198da9dbc3c5a76484ce2a52
Yassa Taiseer <yassataiseer@gmail.com>
2024-01-12 11:14:27 -0500

    revamped firebase cloud functions and added needed changes. implemented a node.js system and rewrote functions into api endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,59 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require('express')
+const cors = require('cors');
+const app = express();
+var serviceAccount = require("./creds.json");
 
-admin.initializeApp();
 
-exports.checkUsernameAvailability = functions.https
-    .onCall(async (data, context) => {
-      const username = data.username;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-      // Check if the username already exists in Firestore
-      const snapshot = await admin.firestore().collection("users")
-          .where("username", "==", username)
-          .limit(1)
-          .get();
 
-      const isUsernameAvailable = snapshot.empty;
+app.use(cors());
 
-      return {isUsernameAvailable};
-    });
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// url endpoint checks if username is available
+// write links like so:
+// http://127.0.0.1:5001/befriend-b14ca/us-central1/app/checkUsernameAvailability?username="Yassa Taiseer"
+//Node: link will be different.
+app.get('/checkUsernameAvailability',async(req,res)=>{
+  
+  const username= req.query.username;
+  const snapshot = await admin.firestore().collection("users")
+  .where("username", "==", username)
+  .limit(1)
+  .get();
+
+  const isUsernameAvailable = snapshot.empty;
+
+  res.send(isUsernameAvailable); 
+});
+
+//
+app.get('/grabAllUsers',async(req,res)=>{
+  let UserList = [];
+  
+  const snapshot = await admin.auth().listUsers()
+  //console.log(snapshot["users"]);
+  for (let element in snapshot["users"]){
+    console.log(element)
+    UserList.push(
+      {
+        "displayName": snapshot["users"][element]["displayName"],
+        "uid": snapshot["users"][element]["uid"],
+      }
+      );
+  }
+  
+
+
+
+
+    exports.app = functions.https.onRequest(app);
